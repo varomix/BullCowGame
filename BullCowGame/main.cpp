c0a92a49da1b2c6fda3ccd8771eb961f7b226244
varomix WEY <varomix@gmail.com>
2016-03-18 00:21:22 -0600

    Getting player input single word

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-	constexpr int WORD_LENGHT = 5;
-	cout << "Welcome to Bulls and Cows" << endl;
+	// introduce the game
+	constexpr int WORD_LENGTH = 5;
+	cout << "Welcome to Bulls and Cows, a fun word game." << endl;
+	cout << "Can you guess the " << WORD_LENGTH;
+	cout << " letter word I'm thinking of?\n\n";
+	 
+	// get a guess from the player
+	string Guess = "";
+	cout << "Enter your guess: ";
+	cin >> Guess;
+
+	cout << "your guess was: " << Guess << endl;
+
+
+
+	cout << endl;
 	return 0;
 }
